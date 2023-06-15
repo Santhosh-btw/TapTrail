c4a7e71f62cdec46298f137e30bcd433682b18d4
Mann-Kurani <mkkurani1@gmail.com>
2023-06-15 10:46:06 +0530

    testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tap_trail/main.dart';
+import 'package:tap_trail/main_fe.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
